1c6bb24c2fb6b21b88ba2c872ca9ef6c23d88371
Andres TheFirst <darkshadowskull@gmail.com>
2017-11-15 21:26:50 -0600

    initial commit

---
+++ src/middlewares/async.js
@@ -0,0 +1,15 @@
+export default function ({ dispatch }) {
+    return next => action => {
+        if(!action.payload || !action.payload.then) {
+            return next(action);
+        }
+
+        action.payload
+            .then(response => {
+                //create a new action with the old type, but
+                //replace the promise with the replace data
+                const newAction = { ...action, payload: response};
+                dispatch(newAction);
+            });
+    }
+}
